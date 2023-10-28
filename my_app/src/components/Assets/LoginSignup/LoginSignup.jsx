cfc74942a48cf7a755fee26e7e6256bff3033326
Rupoum <rupoummitra15278@gmail.com>
2023-10-28 19:50:59 +0530

    Added navbar initial

---
+++ my_app/src/components/Assets/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./LoginSignup.css";
+
+const LoginSignup = () => {
+  return (
+    <div>
+      <header class="header">
+        <a href="#" class="logo">
+          Logo
+        </a>
+        <nav class="navbar">
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Services</a>
+          <a href="#">Contact</a>
+        </nav>
+        <button class="btnLogin">Login</button>
+      </header>
+    </div>
+  );
+};
+
+export default LoginSignup;
